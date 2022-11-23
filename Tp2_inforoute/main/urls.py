638df1861aa788e08cfcad0c887419d166a6bf54
Idriss7734 <80236304+Idriss7734@users.noreply.github.com>
2022-11-23 16:24:38 -0500

    add login api

---
+++ Tp2_inforoute/main/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from .views import register, login
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tp2",
+        default_version="v1.0",
+        description="Application pour les gens qui ont de la difficulté à lire.",
+    ),
+    public=True,
+)
+
+urlpatterns = [
+    path("login/", login, name="login"),
+    path(
+        "swagger",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+]
--- Tp2_inforoute/main/views.py
+++ Tp2_inforoute/main/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+
+from drf_yasg.utils import swagger_auto_schema
+
+from .serializers import LoginSerializer
+
+def register(request):
+    if request.method == "GET":
+        return render(request, "Tp/register.html", {"status": False})
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = User(username=username, password=make_password(password))
+        user.save()
+
+        return render(request, "Tp/register.html", {"status": True})
+
+
+
+
+@swagger_auto_schema(
+    method="post", tags=["Authentication"], request_body=LoginSerializer
+)
+@api_view(["POST"])
+def login(request):
+    username = request.data["username"]
+    password = request.data["password"]
+
+    user = User.objects.filter(username=username)
+
+    if user.exists():
+        auth_success = check_password(password, user.first().password)
+
+        if auth_success:
+            token = Token.objects.filter(user=user.first())
+
+            if token.exists():
+                return Response(
+                    {"token": "Token {}".format(token.first().key)},
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                token = Token.objects.create(user=user.first())
+                return Response(
+                    {"token": "Token {}".format(token.key)}, status=status.HTTP_200_OK
+                )
+        else:
+            return Response(
+                {"message": "Mot de passe incorrect !"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+    else:
+        return Response(
+            {"message": "L'utilisateur n'existe pas !"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
